57d1a7774adf423708d975a0e8152b19d83be2dc
a0916105 <whdofjew@gmail.com>
2021-02-19 15:24:33 +0800

    startActivityForResult

--- app/src/main/java/tw/idv/jew/atm/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/atm/MainActivity.kt
@@ -3,9 +3,14 @@ package tw.idv.jew.atm
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     var login = false
+
+    companion object {
+        val RC_LOGIN = 30
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         if (!login){
             Intent(this, LoginActivity::class.java).apply {
-                startActivity(this)
+//                startActivity(this)
+                startActivityForResult(this, RC_LOGIN)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_LOGIN) {
+            if (resultCode == RESULT_OK) {
+                val userid = data?.getStringExtra("LOGIN_USERID")
+                val passwd = data?.getStringExtra("LOGIN_PASSWD")
+                Log.d("RESULT", "$userid / $passwd")
             }
         }
     }
