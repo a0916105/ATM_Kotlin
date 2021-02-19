33d3b8286c9128681cc29167462264b7af488665
a0916105 <whdofjew@gmail.com>
2021-02-19 17:28:43 +0800

    RecyclerView

---
+++ app/src/main/java/tw/idv/jew/atm/Contact.kt
@@ -0,0 +1,6 @@
+package tw.idv.jew.atm
+
+data class Contact(
+        val name: String,
+        val phone: String
+)
--- app/src/main/java/tw/idv/jew/atm/MaterialActivity.kt
+++ app/src/main/java/tw/idv/jew/atm/MaterialActivity.kt
@@ -3,9 +3,15 @@ package tw.idv.jew.atm
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.contact_row.view.*
+import kotlinx.android.synthetic.main.content_material.*
 
 class MaterialActivity : AppCompatActivity() {
 
@@ -23,5 +29,38 @@ class MaterialActivity : AppCompatActivity() {
                 .setActionTextColor(Color.YELLOW)
                 .show()
         }
+
+        //data
+        val contacts = listOf<Contact>(
+                Contact("Hank", "6661234"),
+                Contact("Jack", "99838882"),
+                Contact("Jenny", "98881234"),
+                Contact("Eric", "77366363"))
+
+        recycler.setHasFixedSize(true)  //固定每列資料的寬高，可提升效能與順暢度
+        recycler.layoutManager = LinearLayoutManager(this)  //垂直清單
+
+        val adapter = object : RecyclerView.Adapter<ContactViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+                val view = layoutInflater.inflate(R.layout.contact_row, parent, false)
+                return ContactViewHolder(view)
+            }
+
+            override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+                holder.name.setText(contacts.get(position).name)
+                holder.phone.setText(contacts.get(position).phone)
+            }
+
+            override fun getItemCount(): Int {
+                return contacts.size
+            }
+        }
+
+        recycler.adapter = adapter
+    }
+
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name = view.contact_name
+        val phone = view.contact_phone
     }
 }
