cb46da9a2f330d1713525056d0b7eb2dc6f11dcb
a0916105 <whdofjew@gmail.com>
2021-02-22 14:59:22 +0800

    CoroutineScope

--- app/src/main/java/tw/idv/jew/atm/ExpenseActivity.kt
+++ app/src/main/java/tw/idv/jew/atm/ExpenseActivity.kt
@@ -2,11 +2,20 @@ package tw.idv.jew.atm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_expense.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
 
 class ExpenseActivity : AppCompatActivity() {
+    companion object {
+        val TAG = ExpenseActivity::class.java.simpleName
+    }
+    private lateinit var database: ExpenseDatabase
+
     val expenseData = arrayListOf<Expense>(
         Expense("2021/02/01", "Lunch", 120),
         Expense("2021/02/02", "停車費", 60),
@@ -17,6 +26,16 @@ class ExpenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense)
+
+        database = Room.databaseBuilder(this,
+                ExpenseDatabase::class.java, "expense.db")
+                .build()
+        //Query expenses
+        CoroutineScope(Dispatchers.IO).launch {
+//        CoroutineScope(Dispatchers.Default).launch {
+            val expenses = database.expenseDao().getAll()
+            Log.d(TAG, expenses.size.toString())
+        }
 
         fab.setOnClickListener {
             val database = Room.databaseBuilder(this,
