d20a13a034c6b4f87fc17ee4a325209faadeae96
a0916105 <whdofjew@gmail.com>
2021-03-02 22:49:22 +0800

    MyLocation

--- app/src/main/java/tw/idv/jew/atm/MapsActivity.kt
+++ app/src/main/java/tw/idv/jew/atm/MapsActivity.kt
@@ -1,7 +1,10 @@
 package tw.idv.jew.atm
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,6 +15,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val REQUEST_LOCATION: Int = 0
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,11 +41,36 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap?.run {
             mMap.uiSettings.isZoomControlsEnabled = true
+
+            setupMap()
         }
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setupMap() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+
+        mMap.setOnMyLocationButtonClickListener { false }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_LOCATION){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                setupMap()
+            }
+        }
     }
 }
